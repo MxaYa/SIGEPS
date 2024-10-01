9b14e846e0e39b22c75fceb5af33be26381eea77
Carlos <carlostechsoftware@hotmail.com>
2024-10-01 20:54:34 -0300

    Terminei o front

--- Implementação/Front-end/src/App.jsx
+++ Implementação/Front-end/src/App.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login'; // Importando o componente de Login
-import Login2 from './components/Login2'; // Importando a nova página de Login2
+import Tela2 from './components/Tela2'; // Importando a nova página de Login2
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} /> {/* Renderizando o componente de Login na rota raiz */}
-        <Route path="/" element={<Login2 />} /> {/* Renderizando o componente Login2 na rota /login2 */}
+      <Route path="/Tela2" element={<Tela2 />} /> {/* Renderizando o componente Login2 na rota /login2 */}
       </Routes>
     </Router>
   );
--- Implementação/Front-end/src/components/Login.jsx
+++ Implementação/Front-end/src/components/Login.jsx
@@ -1,62 +1,59 @@
 import React from 'react';
-import './Login.css'; // Certifique-se de ter esse arquivo CSS disponível
+import './Login.css'; // Certifique-se de ter o CSS adequado
 import { useNavigate } from 'react-router-dom';
-
-
 
 const Login = () => {
   const navigate = useNavigate();
-
-  const handleLoginClick = (e) => {
-    e.preventDefault();
-    navigate('./components/Login2');
+  const entrar = (e) => {
+    e.preventDefault(); // Prevenir o comportamento padrão do formulário
+    navigate('/Tela2'); // Navegar para a página Login2
   };
+
 
   return (
     <div className="container" id="container">
       <div className="form-container sign-up">
         <form>
-          <h1>Create Account</h1>
+          <h1>Bem vindo de volta</h1>
           <div className="social-icons">
             <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
             <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
             <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
             <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
           </div>
-          <span>or use your email for registration</span>
+          <span>Preencha os</span>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <button type="submit">Sign Up</button>
+          <button>Sign Up</button>
         </form>
       </div>
       <div className="form-container sign-in">
         <form>
-          <h1>Criar sua conta</h1>
-          <span>Preencha seus dados abaixo</span>
-          <div className="social-icons">
+          <h1>Entrar</h1>
+          <span>Preencha os campos abaixo</span>
+          <input type="nome" placeholder="Nome" required/>
+        
+           
           
-            
-          </div>
-            
-          <input type="name" placeholder="Nome" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Senha" />
-          <a href="#"></a>
-          <button type="submit">Cadastrar</button>
+          
+          <input type="email" placeholder="Email"  required/>
+          <input type="password" placeholder="Senha" required />
+          <a href="#">Esqueceu sua senha?</a>
+          <button>Login</button>
         </form>
       </div>
       <div className="toggle-container">
         <div className="toggle">
           <div className="toggle-panel toggle-left">
-            <h1>Welcome Back!</h1>
+            <h1>Bem vindo de volta!</h1>
             <p>Enter your personal details to use all of site features</p>
             <button className="hidden" id="login">Sign In</button>
           </div>
           <div className="toggle-panel toggle-right">
-            <h1>Bem vindo de volta</h1>
-            <p>Acesse sua conta agora mesmo!</p>
-            <button className="hidden" id="register" onClick={handleLoginClick}>Entrar</button>
+            <h1>Bem vindo!</h1>
+            <p>Não tem uma conta?Crie uma agora mesmo</p>
+            <button className="hidden" id="register" onClick={entrar}>Cadastre-se</button>
           </div>
         </div>
       </div>
